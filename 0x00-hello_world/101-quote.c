c0d6657c80036088297308199afbe927524752bb
ahmed-abdelgalil <you@example.com>
2023-02-16 10:35:14 -0800

    ..

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print quote to standard error using fprintf()
+ *
+ * Return: one
+ */
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	return (1);
+}
