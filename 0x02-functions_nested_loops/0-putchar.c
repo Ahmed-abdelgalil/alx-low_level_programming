1bddc0cce9c73f013923d76fd62b143117b06289
ahmed-abdelgalil <you@example.com>
2023-02-21 15:02:02 -0800

    ..

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar followed by new line 
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	char ch[] = "_putchar";
+
+	while(ch)
+	{
+		_putchar(ch);
+		ch++
+	}
+	return (0);
+}
