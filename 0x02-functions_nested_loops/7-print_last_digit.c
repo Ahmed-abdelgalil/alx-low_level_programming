bd1216fff1207fc1a74442894fba1a953c1e8543
ahmed-abdelgalil <you@example.com>
2023-02-21 16:55:42 -0800

    ..

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_last_digit - print last digit of number 
+ * @n: integer to test
+ *
+ * Return: absolute value of int 
+ */
+int print_last_digit(int n)
+{
+	return (abs(n)%10);
+}
