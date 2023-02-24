2a7bf983ed313a81b6eaf340c804796bcf692fc9
ahmed-abdelgalil <you@example.com>
2023-02-24 07:19:01 -0800

    ..

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,12 @@
 #include "main.h"
 #include <ctype.h>
 
-/*
+/**
 * _isdigit - function to check is digit or not 
 * @c: digit to chck 
 * 
 * Return: 1 if digit and 0 other 
 */
-
 int _isdigit(int c)
 {
     if (isdigit(c))
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
 * mul- function to multiply two int 
 * @a: first number 
 * @b: second number
